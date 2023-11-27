f8a028fd659356eaf1e059ddf820e0d4f9dc2e1a
RomanKosov <kosovr48@gmail.com>
2023-11-27 23:47:14 +0500

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает
+// на вход целое число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+
+int number = new Random().Next(1,100);
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+if (firstDigit >= secondDigit)
+{
+    Console.WriteLine("Наибольшей цифрой числа: " + number + " является цифра:" + firstDigit);
+}
+else
+{
+    Console.WriteLine("Наибольшей цифрой числа: " + number + " является цифра:" + secondDigit);
+}
