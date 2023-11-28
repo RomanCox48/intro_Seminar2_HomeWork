25f45b2039eb4e2ae82d97c9f671486cd1ce8bbb
RomanKosov <kosovr48@gmail.com>
2023-11-28 21:32:07 +0500

    task4LastVersion

--- task4/Program.cs
+++ task4/Program.cs
@@ -14,29 +14,34 @@ class Program
     {
         int reversedNumber = 0;
 
+        // Переменная для отслеживания количества выведенных цифр
+        int digitCount = 0;
+
         // Используем цикл для извлечения и вывода цифр в обратном порядке
         while (n > 0)
         {
             int digit = n % 10;
             reversedNumber = reversedNumber * 10 + digit;
             n /= 10;
+            digitCount++;
         }
 
         // Используем цикл для извлечения и вывода цифр с запятой
-        while (reversedNumber > 0)
+        while (digitCount > 0)
         {
             int digit = reversedNumber % 10;
             Console.Write(digit);
 
             // Если это не последняя цифра, выводим запятую
-            if (reversedNumber >= 10)
+            if (digitCount > 1)
             {
                 Console.Write(",");
             }
 
             reversedNumber /= 10;
+            digitCount--;
         }
 
         Console.WriteLine();
     }
-} 
+}
