193128c80a1a3516deaf56685a41aaa5ebd31b1e
RomanKosov <kosovr48@gmail.com>
2023-11-28 21:22:37 +0500

    task4Version2

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,25 +1,42 @@
-﻿using System.Data;
+﻿using System;
 
-Console.Write("Введите натуральное число N: ");
-int N = Convert.ToInt32(Console.ReadLine());
-
-if (N < 10)
+class Program
 {
-    Console.WriteLine(N);
-}
-else
-{
-    while (N > 0)
+    static void Main()
     {
-        int currentDigit = N % 10;
-        N /= 10;
-        if (N > 0)
-        {
-            Console.WriteLine(currentDigit + ",");
-        }
-        else
-        {
-            Console.WriteLine(currentDigit);
-        }
+        Console.Write("Введите натуральное число N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        ShowDigitsReverse(N);
     }
-}
+
+    static void ShowDigitsReverse(int n)
+    {
+        int reversedNumber = 0;
+
+        // Используем цикл для извлечения и вывода цифр в обратном порядке
+        while (n > 0)
+        {
+            int digit = n % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            n /= 10;
+        }
+
+        // Используем цикл для извлечения и вывода цифр с запятой
+        while (reversedNumber > 0)
+        {
+            int digit = reversedNumber % 10;
+            Console.Write(digit);
+
+            // Если это не последняя цифра, выводим запятую
+            if (reversedNumber >= 10)
+            {
+                Console.Write(",");
+            }
+
+            reversedNumber /= 10;
+        }
+
+        Console.WriteLine();
+    }
+} 
