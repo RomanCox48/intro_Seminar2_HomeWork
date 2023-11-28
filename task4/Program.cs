b7e328f1f4e3cf05843b1b341e4f44bc47654543
RomanKosov <kosovr48@gmail.com>
2023-11-28 20:48:34 +0500

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+
+Console.Write("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 10)
+{
+    Console.WriteLine(N);
+}
+else
+{
+    while (N > 0)
+    {
+        int currentDigit = N % 10;
+        N /= 10;
+        if (N > 0)
+        {
+            Console.WriteLine(currentDigit + ",");
+        }
+        else
+        {
+            Console.WriteLine(currentDigit);
+        }
+    }
+}
